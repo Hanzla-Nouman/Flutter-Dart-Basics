187034ff6a2a585a65fdcdcab6ba4112b25015f2
Hanzla-Nouman <hanzlanouman514@gmail.com>
2024-08-18 15:07:37 +0500

    first commit

---
+++ lib/pr.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+void main() {
+
+  // Variables and Datatypes
+  print("Welcome to Dart!!");
+
+  var lang = "Flutter";
+  print(lang);
+
+  int? a = 45;
+  print(a);
+
+  String greet = 'Hello!';
+  print(greet);
+
+  bool isLogin = true;
+  print(isLogin);
+
+  //  stdout.write("Name: ");
+  //  var name =  stdin.readLineSync();
+  //  print("Name is $name");
+
+ // Dynamically storing data
+  dynamic response = 4;
+  response = true;
+  response = "Hanlza";
+  print(response);
+
+  var res;
+  res = true;
+  res = "Hanlza";
+  print(res); 
+}
