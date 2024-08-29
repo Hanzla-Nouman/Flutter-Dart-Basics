4e974708cdda4d2ba6660ea3655759794c382fd9
Hanzla-Nouman <hanzlanouman514@gmail.com>
2024-08-29 23:40:42 +0500

    Understanding concepts of widgets

--- lib/main.dart
+++ lib/main.dart
@@ -59,6 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'times:',
             ),
+           
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
