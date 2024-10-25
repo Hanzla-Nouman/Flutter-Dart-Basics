f11cc117912202a3728f45311838e80ad38d0666
Hanzla-Nouman <hanzlanouman514@gmail.com>
2024-10-25 23:59:23 +0500

    Using Draggable Widget

--- lib/hero.dart
+++ lib/hero.dart
@@ -5,46 +5,69 @@ class HeroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.brown,
-        body:SingleChildScrollView(child:  Column(
-      children: [
-        
-        
-             Hero(
+        backgroundColor: Colors.brown,
+        body: SingleChildScrollView(
+            child: Column(
+          children: [
+            Hero(
               transitionOnUserGestures: true,
               tag: "background",
               child: ClipRRect(
                 child: Image.asset('assets/images/github.jpg'),
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20)),
               ),
             ),
-        Container(
-          height: 300,
-          child: ListWheelScrollView(
-              itemExtent: 100,
-              children: dummyData.map((v) {
-                return Container(
-                  decoration: BoxDecoration(
-                      color: Colors.blueAccent,
-                      borderRadius: BorderRadius.circular(10)),
-                  width: 300,
-                  child: Center(
-                    child: Text(
-                      v,
-                      style: TextStyle(fontSize: 23, color: Colors.white),
-                    ),
-                  ),
-                );
-              }).toList()),
-        )
-      ],
-    )),
-    bottomNavigationBar: BottomNavigationBar(type: BottomNavigationBarType.fixed,currentIndex: 2 ,unselectedItemColor: Colors.black38,selectedItemColor: Colors.blue, backgroundColor: Colors.lightGreenAccent,items: [
-      BottomNavigationBarItem(icon: Icon(Icons.home),label: 'Home'),
-      BottomNavigationBarItem(icon: Icon(Icons.contacts),label: 'Contact'),
-      BottomNavigationBarItem(icon: Icon(Icons.phone),label: 'Call'),
-      BottomNavigationBarItem(icon: Icon(Icons.call_end_outlined),label: 'End'),
-    ])
-    );
+            SizedBox(
+              height: 100,
+              width: double.infinity,
+              child: Row(
+                children: [
+                  Draggable(
+                      child: Container(
+                        width: 90,
+                        height: 90,
+                        color: Colors.blueAccent,
+                      ),
+                      feedback: Container())
+                ],
+              ),
+            ),
+            Container(
+              height: 300,
+              child: ListWheelScrollView(
+                  itemExtent: 100,
+                  children: dummyData.map((v) {
+                    return Container(
+                      decoration: BoxDecoration(
+                          color: Colors.blueAccent,
+                          borderRadius: BorderRadius.circular(10)),
+                      width: 300,
+                      child: Center(
+                        child: Text(
+                          v,
+                          style: TextStyle(fontSize: 23, color: Colors.white),
+                        ),
+                      ),
+                    );
+                  }).toList()),
+            )
+          ],
+        )),
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            currentIndex: 2,
+            unselectedItemColor: Colors.black38,
+            selectedItemColor: Colors.blue,
+            backgroundColor: Colors.lightGreenAccent,
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.contacts), label: 'Contact'),
+              BottomNavigationBarItem(icon: Icon(Icons.phone), label: 'Call'),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.call_end_outlined), label: 'End'),
+            ]));
   }
 }
