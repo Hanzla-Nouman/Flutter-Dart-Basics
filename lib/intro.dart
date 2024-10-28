cdb75b9e668a7fb2853ed131822d1ec3e780c278
Hanzla-Nouman <hanzlanouman514@gmail.com>
2024-10-28 22:51:59 +0500

    AlertDialog widget

--- lib/intro.dart
+++ lib/intro.dart
@@ -10,17 +10,38 @@ class IntroPage extends StatelessWidget {
       body: Container(
         color: Colors.brown,
         child: Center(
-          child: ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => MyHomePage(
-                              title: 'Main Page',
-                            )));
-              },
-              child: Text('Home Screen $a')),
-        ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => MyHomePage(
+                                title: 'Main Page',
+                              )));
+                },
+                child: Text('Home Screen $a')),
+            ElevatedButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                            actions: [
+                              TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: Text('Close'))
+                            ], content: Text('This is alert dialog content'),
+                            contentPadding: EdgeInsets.all(20) ,
+                            title: Text('Flutter'),
+                          ));
+                },
+                child: Text('Show Dialog'))
+          ],
+        )),
       ),
     );
   }
